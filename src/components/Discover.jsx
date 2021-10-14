a517fe4f9c7c8b0f34ac34866168e69ace393d34
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-14 21:43:26 +0300

    added 4 discover images

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../styles/discover.css";
 import feelgood1 from "../assets/feelgood1.jpg";
-// import feelgood2 from "../assets/feelgood2.jpg";
-// import feelgood3 from "../assets/feelgood3.jpg";
-// import feelgood4 from "../assets/feelgood4.jpg";
+import feelgood2 from "../assets/feelgood2.jpg";
+import feelgood3 from "../assets/feelgood3.jpg";
+import feelgood4 from "../assets/feelgood4 .jpg";
 
 const Discover = () => {
   return (
@@ -12,8 +12,35 @@ const Discover = () => {
         Discover the programs that have helped thousands heal their mind and
         transform their life
       </h1>
-      <div className="discoverImgContainer">
-        <img className="discoverImg" src={feelgood1} alt="" />
+      <div id="discoverWrapper">
+        <div className="discoverImgContainer">
+          <img className="discoverImg" src={feelgood1} alt="" />
+          <p className="discoverPara">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui labore
+            sapiente soluta nihil cumque
+          </p>
+        </div>
+        <div className="discoverImgContainer">
+          <img className="discoverImg" src={feelgood2} alt="" />
+          <p className="discoverPara">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui labore
+            sapiente soluta nihil cumque?
+          </p>
+        </div>
+        <div className="discoverImgContainer">
+          <img className="discoverImg" src={feelgood3} alt="" />
+          <p className="discoverPara">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui labore
+            sapiente soluta nihil cumque?
+          </p>
+        </div>
+        <div className="discoverImgContainer">
+          <img className="discoverImg" src={feelgood4} alt="" />
+          <p className="discoverPara">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui labore
+            sapiente soluta nihil cumque?
+          </p>
+        </div>
       </div>
     </div>
   );
