ac15089dd801991f2be8f96b5a7ba4f211ba10c3
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-15 15:22:20 +0300

    added the rest of the services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,28 +2,85 @@ import React from "react";
 import "../styles/services.css";
 import happyPerson from "../assets/happyPerson.jpg";
 import { Button, ButtonGroup } from "@chakra-ui/react";
+import goodFood from "../assets/goodFood.jpg";
+import happyMeditation from "../assets/happyMeditation.jpg";
+import homeComfort from "../assets/homeComfort.jpg";
 
 const WaysToWork = () => {
   return (
     <div id="servicesComponentContainer">
       <h1 id="servicesTitle">----My Services----</h1>
       <div id="servicesContainer">
+        {/* service 1 */}
         <div className="service">
-          <div className="leftSide">
-            <h1 className="serviceTitle">Lorem ipsum</h1>
+          <div className="imgPart">
+            <h1 className="serviceTitle">Positive Life</h1>
             <div className="serviceImgContainer">
               <img className="serviceImage" src={happyPerson} alt="" />
             </div>
           </div>
-          <div className="rightSide">
+          <div className="textPart">
             <p className="servicePara">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quo
               cum, nulla totam quibusdam ducimus aperiam aliquid sed error
               facilis laboriosam, obcaecati earum quisquam reprehenderit quaerat
               quae a ullam repellat.
             </p>
-
             <Button colorScheme="orange">FIND OUT HOW</Button>
+          </div>
+        </div>
+        {/* service 2 */}
+        <div className="service">
+          <div className="textPart" id="bluePart">
+            <p className="servicePara">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quo
+              cum, nulla totam quibusdam ducimus aperiam aliquid sed error
+              facilis laboriosam, obcaecati earum quisquam reprehenderit quaerat
+              quae a ullam repellat.
+            </p>
+            <Button colorScheme="orange">FIND OUT HOW</Button>
+          </div>
+          <div className="imgPart">
+            <h1 className="serviceTitle">Good Food</h1>
+            <div className="serviceImgContainer">
+              <img className="serviceImage" src={goodFood} alt="" />
+            </div>
+          </div>
+        </div>
+        {/* service 3 */}
+        <div className="service">
+          <div className="imgPart">
+            <h1 className="serviceTitle">Free Your Mind</h1>
+            <div className="serviceImgContainer">
+              <img className="serviceImage" src={happyMeditation} alt="" />
+            </div>
+          </div>
+          <div className="textPart">
+            <p className="servicePara">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quo
+              cum, nulla totam quibusdam ducimus aperiam aliquid sed error
+              facilis laboriosam, obcaecati earum quisquam reprehenderit quaerat
+              quae a ullam repellat.
+            </p>
+            <Button colorScheme="orange">FIND OUT HOW</Button>
+          </div>
+        </div>
+        {/* service 4 */}
+        <div className="service">
+          <div className="textPart" id="bluePart">
+            <p className="servicePara">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quo
+              cum, nulla totam quibusdam ducimus aperiam aliquid sed error
+              facilis laboriosam, obcaecati earum quisquam reprehenderit quaerat
+              quae a ullam repellat.
+            </p>
+            <Button colorScheme="orange">FIND OUT HOW</Button>
+          </div>
+          <div className="imgPart">
+            <h1 className="serviceTitle">Work Remotely</h1>
+            <div className="serviceImgContainer">
+              <img className="serviceImage" src={homeComfort} alt="" />
+            </div>
           </div>
         </div>
       </div>
